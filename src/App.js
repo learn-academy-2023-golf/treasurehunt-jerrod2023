95cb08496d35e7054056e5ef5d1eb41d6d0d00ad
Jerrod <chaddjed@gmail.com>
2023-10-24 14:55:24 -0700

    add winner and loser

--- src/App.js
+++ src/App.js
@@ -14,11 +14,25 @@ const App = () => {
     "?",
     "?"
   ])
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+
+  console.log("treasure:", treasureLocation)
+  console.log("bomb:", bombLocation)
+
   const handleGamePlay = (index) => {
     // alert(index)
     let updatedBoard = [...board]
-    updatedBoard[index] = "🌴"
+    if(treasureLocation === index) {
+      updatedBoard[index] = "💎"
+      setBoard(updatedBoard)
+    }else if(bombLocation === index) {
+      updatedBoard[index] = "💣"
+      setBoard(updatedBoard)
+    } else {updatedBoard[index] = "🌴"
     setBoard(updatedBoard)
+    }
+
   }
 
   return (
